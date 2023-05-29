77361ee732a18439b6291f7bb0eef0002a6ec54d
Artur J. Żarek <34176570+ajz3d@users.noreply.github.com>
2023-05-29 15:10:39 +0200

    Bump version

--- scripts/python/houclip/__init__.py
+++ scripts/python/houclip/__init__.py
@@ -3,6 +3,6 @@
 __author__ = 'Artur J. Żarek'
 __copyright__ = 'Copyright 2023, Artur J. Żarek'
 __license__ = 'GNU GPL version 3'
-__version__ = '0.50'
+__version__ = '0.5.1'
 __maintainer__ = 'Artur J. Żarek'
 __status__ = 'Development'
